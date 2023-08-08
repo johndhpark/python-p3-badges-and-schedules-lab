4b161642370efd9457d58998db179b4b26b5021d
John DH. Park <john.dh.park@icloud.com>
2023-08-08 05:52:21 -0700

    Completed Badges and Schedule Lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,16 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {index + 1}!" for index, name in enumerate(names)]
+
 
 def printer(names):
-    return None
+    res = batch_badge_creator(names) + assign_rooms(names)
+
+    for message in res:
+        print(message)
+
